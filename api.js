826013cad00f92cf968920385cfa0f2a5406e61c
alienrusheur <ranranbous@gmail.com>
2026-06-24 15:53:51 +0200

    Ajout de fetchMeteo sur api

--- api.js
+++ api.js
@@ -1,3 +1,21 @@
-function penis(cm, diametre) {
-    console.log("oui")
+const Baseurl = "https://api.open-meteo.com/v1/forecast";
+
+function fetchMeteo(lat, lon) {
+
+
+current: {
+temperature_2m: 15.2;
+weathercode: 3;
+windspeed_10m: 12.4;
+relativehumidity_2m: 72;
+};
+
+daily: {
+time: "2025-03-15", "2025-03-16";
+temperature_2m_max: 18, 16;
+temperature_2m_min: 10, 8;
 }
+    
+}
+
+console.log(current);
